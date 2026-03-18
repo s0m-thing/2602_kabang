744470b87eb12aa3918dd7721be313a72de6f2cc
s0m-thing <gkstha0712@gmail.com>
2026-03-18 10:01:54 +0900

    병합 수정

--- script.js
+++ script.js
@@ -3,158 +3,158 @@ console.log("test");
 // 헤더 부분 스크립트
 function Header__init () {
     let $dropDownContent = $('.drop_down_content');
-		let $dropBtn = $('.drop_btn');
+    let $dropBtn = $('.drop_btn');
 
-		$dropBtn.click(function() {		
-			$dropDownContent.toggleClass('is-active');
-			$dropBtn.toggleClass('rotate');
-		});
+    $dropBtn.click(function() {		
+        $dropDownContent.toggleClass('is-active');
+        $dropBtn.toggleClass('rotate');
+    });
 
-		$(window).click(function(e) {
-			if ( !$(e.target).closest('.drop_btn').length ) {
-				$dropDownContent.removeClass('is-active');
-				$dropBtn.removeClass('rotate');
-			}
-		});
+    $(window).click(function(e) {
+        if ( !$(e.target).closest('.drop_btn').length ) {
+            $dropDownContent.removeClass('is-active');
+            $dropBtn.removeClass('rotate');
+        }
+    });
 
-		$('header .menu_box > ul > li').mouseenter(function() {
-			$dropDownContent.removeClass('is-active');
-			$dropBtn.removeClass('rotate');
-		});
+    $('header .menu_box > ul > li').mouseenter(function() {
+        $dropDownContent.removeClass('is-active');
+        $dropBtn.removeClass('rotate');
+    });
 }
 
 Header__init();
 
 
 /* 발견되면 활성화시키는 라이브러리 시작 */
-        function ActiveOnVisible__init() {
-        $('.active-on-visible').each(function(index, node) {
-            var $node = $(node);
+function ActiveOnVisible__init() {
+    $('.active-on-visible').each(function(index, node) {
+        var $node = $(node);
 
-            var onFuncName = $node.attr('data-active-on-visible-on-func-name');
-            var offFuncName = $node.attr('data-active-on-visible-off-func-name');
+        var onFuncName = $node.attr('data-active-on-visible-on-func-name');
+        var offFuncName = $node.attr('data-active-on-visible-off-func-name');
 
-            $node.data('data-active-on-visible-on-func-name', onFuncName);
-            $node.data('data-active-on-visible-off-func-name', offFuncName);
-        });
+        $node.data('data-active-on-visible-on-func-name', onFuncName);
+        $node.data('data-active-on-visible-off-func-name', offFuncName);
+    });
 
-        $(window).resize(_.debounce(ActiveOnVisible__initOffset, 500));
-        ActiveOnVisible__initOffset();
+    $(window).resize(_.debounce(ActiveOnVisible__initOffset, 500));
+    ActiveOnVisible__initOffset();
 
-        $(window).scroll(_.debounce(ActiveOnVisible__checkAndActive, 50));
-        ActiveOnVisible__checkAndActive();
+    $(window).scroll(_.debounce(ActiveOnVisible__checkAndActive, 50));
+    ActiveOnVisible__checkAndActive();
+}
+
+function ActiveOnVisible__initOffset() {
+    var windowHeight = $(window).height();
+
+    $('.active-on-visible:not(.actived)').each(function(index, node) {
+        var $node = $(node);
+
+        var offsetTop = $node.offset().top;
+
+        var matrix = $node.css('transform').replace(/[^0-9\-.,]/g, '').split(',')
+        var translateX = matrix[12] || matrix[4];
+        var translateY = matrix[13] || matrix[5];
+
+        if ( translateY ) {
+        offsetTop -= translateY;
         }
 
-        function ActiveOnVisible__initOffset() {
+        $node.attr('data-active-on-visible-offsetTop', offsetTop);
+        $node.data('data-active-on-visible-offsetTop', offsetTop);
+
+        if ( !$node.attr('data-active-on-visible-diff-y') ) {
+        $node.attr('data-active-on-visible-diff-y', '0');
+        }
+
+        if ( !$node.attr('data-active-on-visible-delay') ) {
+        $node.attr('data-active-on-visible-delay', '0');
+        }
+
+        var diffY = $node.attr('data-active-on-visible-diff-y');
+        var delay = $node.attr('data-active-on-visible-delay');
+
+        if ( diffY.substr(-2, 2) == 'vh' ) {
+        diffY = parseInt(diffY);
+
+        diffY = windowHeight * (diffY / 100);
+        }
+        else if ( diffY.substr(-1, 1) == '%' ) {
+        diffY = parseInt(diffY);
+
+        diffY = $node.height() * (diffY / 100);
+        }
+        else {
+        diffY = parseInt(diffY);
+        }
+
+        $node.attr('data-active-on-visible-diff-y-real', diffY);
+        $node.data('data-active-on-visible-diff-y', diffY);
+        $node.data('data-active-on-visible-delay', delay);
+    });
+
+    ActiveOnVisible__checkAndActive();
+}
+
+function ActiveOnVisible__checkAndActive() {
+    $('.active-on-visible:not(.actived)').each(function(index, node) {
+        var $node = $(node);
+
+        var offsetTop = $node.data('data-active-on-visible-offsetTop') * 1;
+        var diffY = parseInt($node.data('data-active-on-visible-diff-y'));
+        var delay = parseInt($node.data('data-active-on-visible-delay'));
+
+        var onFuncName = $node.data('data-active-on-visible-on-func-name');
+        var offFuncName = $node.data('data-active-on-visible-off-func-name');
+
+        var scrollTop = $(window).scrollTop();
         var windowHeight = $(window).height();
+        var nodeHeight = $node.height();
 
-        $('.active-on-visible:not(.actived)').each(function(index, node) {
-            var $node = $(node);
-
-            var offsetTop = $node.offset().top;
-
-            var matrix = $node.css('transform').replace(/[^0-9\-.,]/g, '').split(',')
-            var translateX = matrix[12] || matrix[4];
-            var translateY = matrix[13] || matrix[5];
-
-            if ( translateY ) {
-            offsetTop -= translateY;
-            }
-
-            $node.attr('data-active-on-visible-offsetTop', offsetTop);
-            $node.data('data-active-on-visible-offsetTop', offsetTop);
-
-            if ( !$node.attr('data-active-on-visible-diff-y') ) {
-            $node.attr('data-active-on-visible-diff-y', '0');
-            }
-
-            if ( !$node.attr('data-active-on-visible-delay') ) {
-            $node.attr('data-active-on-visible-delay', '0');
-            }
-
-            var diffY = $node.attr('data-active-on-visible-diff-y');
-            var delay = $node.attr('data-active-on-visible-delay');
-
-            if ( diffY.substr(-2, 2) == 'vh' ) {
-            diffY = parseInt(diffY);
-
-            diffY = windowHeight * (diffY / 100);
-            }
-            else if ( diffY.substr(-1, 1) == '%' ) {
-            diffY = parseInt(diffY);
-
-            diffY = $node.height() * (diffY / 100);
-            }
-            else {
-            diffY = parseInt(diffY);
-            }
-
-            $node.attr('data-active-on-visible-diff-y-real', diffY);
-            $node.data('data-active-on-visible-diff-y', diffY);
-            $node.data('data-active-on-visible-delay', delay);
-        });
-
-        ActiveOnVisible__checkAndActive();
-        }
-
-        function ActiveOnVisible__checkAndActive() {
-        $('.active-on-visible:not(.actived)').each(function(index, node) {
-            var $node = $(node);
-
-            var offsetTop = $node.data('data-active-on-visible-offsetTop') * 1;
-            var diffY = parseInt($node.data('data-active-on-visible-diff-y'));
-            var delay = parseInt($node.data('data-active-on-visible-delay'));
-
-            var onFuncName = $node.data('data-active-on-visible-on-func-name');
-            var offFuncName = $node.data('data-active-on-visible-off-func-name');
-
-            var scrollTop = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            var nodeHeight = $node.height();
-
-            if ( scrollTop + windowHeight + diffY > offsetTop ) {
+        if ( scrollTop + windowHeight + diffY > offsetTop ) {
             setTimeout(function() {
                 if ( $node.hasClass('active') == false ) {
                 $node.addClass('active');
 
-          if ( $node.hasClass('can-active-once') ) {
-            $node.addClass('actived');
-          }
+                if ( $node.hasClass('can-active-once') ) {
+                    $node.addClass('actived');
+                }
 
-          if ( window[onFuncName] ) {
-            window[onFuncName]($node);
-          }
+                if ( window[onFuncName] ) {
+                    window[onFuncName]($node);
+                }
+                }
+            }, delay);
         }
-      }, delay);
-    }
-    else {
-      if ( $node.hasClass('active') ) {
-        $node.removeClass('active');
+        else {
+        if ( $node.hasClass('active') ) {
+            $node.removeClass('active');
 
-        if ( window[offFuncName] ) {
-          window[offFuncName]($node);
+            if ( window[offFuncName] ) {
+            window[offFuncName]($node);
+            }
         }
-      }
+        }
+    });
     }
-  });
-}
 
-$(function() {
-  ActiveOnVisible__init();
-})
+    $(function() {
+    ActiveOnVisible__init();
+    })
 
 
 // 체크카드 슬라이드
 function SliderBox1__init() {
-        $('.slider_box > .slick').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            centerMode:true,
-            centerPadding:0,
-            arrows:true,
-            prevArrow: ".slider_box > .arrows > .btn_arrow_left",
-            nextArrow: ".slider_box > .arrows > .btn_arrow_right",
-        });
-        }
+    $('.slider_box > .slick').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode:true,
+        centerPadding:0,
+        arrows:true,
+        prevArrow: ".slider_box > .arrows > .btn_arrow_left",
+        nextArrow: ".slider_box > .arrows > .btn_arrow_right",
+    });
+}
 
-        SliderBox1__init();
+SliderBox1__init();
